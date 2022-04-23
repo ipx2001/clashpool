ba367d2a9312c06e9ff57548161a76b80eec1166
wally <wally94@163.com>
2022-04-23 23:01:47 +0800

    111

--- test.js
+++ test.js
@@ -1,8 +1,14 @@
 var child_process = require('child_process');
 child_process.execSync("chmod +x /home/runner/work/clashpool/clashpool/utils/clash")
-var echo = child_process.spawn('/home/runner/work/clashpool/clashpool/utils/clash', ['-d',"."]);
+var test= child_process.exec("/home/runner/work/clashpool/clashpool/utils/clash -d .",(err,stdout,stderr)=>{
+    if(err){
+        console.log(err);
+    }
+    console.log(stdout);
+})
 
-echo.stdout.on('data', function(data){
-   console.log(data);
-});
 
+setTimeout(()=>{
+    test.kill()
+    console.log("000000");
+},10000)
