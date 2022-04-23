b3f7aa979f74abab28a71e423cccc0debbe963c9
wally <wally94@163.com>
2022-04-23 23:24:13 +0800

    00

--- test.js
+++ test.js
@@ -1,15 +1,9 @@
 #! /usr/bin/env node
-var child_process = require('child_process');
 
-var test= child_process.exec("/home/runner/work/clashpool/clashpool/utils/clash -d .",(err,stdout,stderr)=>{
-    if(err){
-        console.log(err);
-    }
-    console.log(stdout);
-})
+
 
 
 setTimeout(()=>{
-    test.kill()
+ 
     console.log("000000");
 },10000)
