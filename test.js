63e041a7fc66886e40222b980813efe46881e8b7
wally <wally94@163.com>
2022-04-23 22:56:12 +0800

    00

--- test.js
+++ test.js
@@ -1,5 +1,5 @@
 var child_process = require('child_process');
-
+child_process.execSync("chmod +x /home/runner/work/clashpool/clashpool/utils/clash")
 var echo = child_process.spawn('/home/runner/work/clashpool/clashpool/utils/clash', ['-d',"."]);
 
 echo.stdout.on('data', function(data){
