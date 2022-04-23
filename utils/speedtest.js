ec6b90188fa5a0309c67e101c74452dc4e87a86b
wally <wally94@163.com>
2022-04-23 22:43:51 +0800

    0

--- utils/speedtest.js
+++ utils/speedtest.js
@@ -1,5 +1,6 @@
-const execSync = require('child_process').execSync;
-const exec = require('child_process').exec;
+const {spawn} = require('child_process')
+
+const child = spawn("pwd")
 
 /**
  * 本来想用node重新写的，后来因为工作忙，直接调用别人写好的  不过是python版本，用子进程实现调用
