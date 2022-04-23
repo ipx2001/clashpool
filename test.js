8799d4de172dca38bfcd399110117f93333d319b
wally <wally94@163.com>
2022-04-23 23:07:52 +0800

    00

--- test.js
+++ test.js
@@ -1,5 +1,5 @@
 var child_process = require('child_process');
-child_process.execSync("chmod +x /home/runner/work/clashpool/clashpool/utils/clash")
+
 var test= child_process.exec("/home/runner/work/clashpool/clashpool/utils/clash -d .",(err,stdout,stderr)=>{
     if(err){
         console.log(err);
